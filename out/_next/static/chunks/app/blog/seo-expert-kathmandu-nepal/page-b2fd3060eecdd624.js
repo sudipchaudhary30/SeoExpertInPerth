42c3179b51cee123bb1c71402b3b066611849583
Sudip Chaudhary <chsudip30@gmail.com>
2026-06-21 22:34:10 +0545

    menu update

--- out/_next/static/chunks/app/blog/seo-expert-kathmandu-nepal/page-b2fd3060eecdd624.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[219,409,313,31,659],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- out/_next/static/chunks/app/shopify-seo-nepal/page-78f23d50ad3d18a6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[793,160,301,457,444,404,979,49,687,327,437,454,822,576,750,469,860,334,308],{7145:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7145)}),_N_E=n.O()}]);
